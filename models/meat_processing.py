61628c74c45438b4b5fefcc8fc0933cdcda5b2d1
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-08-02 09:58:53 -0600

    Up

--- models/meat_processing.py
+++ models/meat_processing.py
@@ -5,3 +5,10 @@ class MeatProcessingOrder(models.Model):
     _description = 'Meat Processing Order'
 
     name = fields.Char(string='Order Name')
+
+class MeatProcessingOrderLine(models.Model):
+    _name = 'meat.processing.order.line'
+    _description = 'Meat Processing Order Line'
+
+    name = fields.Char(string='Order Line Name')
+    order_id = fields.Many2one('meat.processing.order', string='Order')
