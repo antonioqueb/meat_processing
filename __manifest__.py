55e3c052acc9cf926f5c0a7967e75d31af362f14
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-08-03 20:12:54 -0600

    Up

--- __manifest__.py
+++ __manifest__.py
@@ -7,13 +7,13 @@
         Module for managing meat processing orders including order creation, 
         processing, and finalization.
     """,
-    'depends': ['base', 'stock', 'product', 'mrp'],
+    'depends': ['base', 'stock', 'product', 'mrp', 'web'],
     'data': [
         'security/ir.model.access.csv',
         'data/ir_sequence_data.xml',
         'views/meat_processing_views.xml',
         'views/meat_processing_menu.xml',
-        'views/meat_processing_assets.xml',  # Añadir el archivo de assets
+        'views/meat_processing_assets.xml',
     ],
     'installable': True,
     'application': True,
