4ec9e5eb6fca3f9099a7c33947b6f678496a23fd
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-08-03 20:30:47 -0600

    Up

--- __manifest__.py
+++ __manifest__.py
@@ -13,14 +13,8 @@
         'data/ir_sequence_data.xml',
         'views/meat_processing_views.xml',
         'views/meat_processing_menu.xml',
+        'views/meat_processing_assets.xml',
     ],
-    'assets': {
-        'web.assets_backend': [
-            'meat_processing/static/src/css/meat_processing_styles.css',
-            'meat_processing/static/src/css/kanban_color.css',
-            'meat_processing/static/src/js/kanban_color.js',
-        ],
-    },
     'installable': True,
     'application': True,
     'auto_install': False,
