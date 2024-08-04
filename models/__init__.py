fcba0e6957528e171b173de63e6a4f6ac0fce868
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-08-04 01:01:13 -0600

    Up

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,2 @@
 # meat_processing/models/__init__.py
-from . import meat_processing_order
-from . import meat_processing_order_line
-from . import compute_methods
-from . import stock_move_methods
+from . import meat_processing
