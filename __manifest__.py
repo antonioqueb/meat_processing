c9fb9c060e1a36d3e48a02b451fe1f6ccd6d2959
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-08-03 21:40:11 -0600

    Up

--- __manifest__.py
+++ __manifest__.py
@@ -21,9 +21,7 @@
     'auto_install': False,
     'assets': {
         'web.assets_backend': [
-            'meat_processing/static/src/css/meat_processing_styles.css',
-            'meat_processing/static/src/css/kanban_color.css',
-            'meat_processing/static/src/js/kanban_color.js',
+            'meat_processing/static/src/js/kanban_color.js'
         ],
     },
 }
