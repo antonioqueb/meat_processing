2d97bbfd64fa5618677e6b568ec53a501583b910
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-08-03 22:12:44 -0600

    Up

--- static/src/js/kanban_color.js
+++ static/src/js/kanban_color.js
@@ -1,4 +1,3 @@
-// meat_processing/static/src/js/kanban_color.js
 odoo.define('meat_processing.kanban_color', function (require) {
     "use strict";
 
@@ -9,6 +8,13 @@ odoo.define('meat_processing.kanban_color', function (require) {
             this._super.apply(this, arguments);
             var state = this.recordData.state;
             this.$el.addClass('o_kanban_color_' + state);
+            this.$el.hover(
+                function() {
+                    $(this).css("box-shadow", "0 6px 12px rgba(0, 0, 0, 0.2)");
+                }, function() {
+                    $(this).css("box-shadow", "0 4px 8px rgba(0, 0, 0, 0.1)");
+                }
+            );
         }
     });
 });
